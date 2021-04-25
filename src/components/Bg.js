12e4bd51d853a297ede777ea8214292512f707db
bockmms <bockol@media-saturn.com>
2021-04-25 21:38:40 +0200

    :melon: new Background Image

--- src/components/Bg.js
+++ src/components/Bg.js
@@ -1,2 +1,13 @@
+import React, { Component } from "react";
+import logo from "./bm.jpg";
 
+import "./Bg.css";
+
+export default class home extends Component {
+  render() {
+    return (
+        <div className="container-fluid homepage-bgimage"></div>
+    );
+  }
+}
 
